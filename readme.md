1d97cebce456b504d2bd3eb0e52e6e2ac27f7042
Hiromi Kohara <h_kohara@me.com>
2020-05-21 20:28:12 -0700

    travis

---
+++ readme.md
@@ -0,0 +1,6 @@
+[![codecov](https://codecov.io/gh/kokutaro/webex-bot-tester/branch/master/graph/badge.svg)](https://codecov.io/gh/kokutaro/webex-bot-tester)
+[![Build Status](https://travis-ci.org/kokutaro/webex-bot-tester.png)](https://travis-ci.org/kokutaro/webex-bot-tester)
+
+## WebEx Bot Tester
+
+Make WebEx BOT easy to use
--- src/components/RoomComponent.tsx
+++ src/components/RoomComponent.tsx
@@ -1,11 +1,13 @@
-import { IconButton } from '@material-ui/core';
+import { CircularProgress, IconButton } from '@material-ui/core';
 import { MoreHoriz } from '@material-ui/icons';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getRooms } from '../actions/RoomActions';
+import { getTeams } from '../actions/TeamActions';
 import { RoomState } from '../states/RoomState';
 import { State } from '../states/State';
+import { TeamState } from '../states/TeamState';
 import { styled } from './FoundationStyles';
 
 const Mono = styled.pre`
@@ -32,12 +34,22 @@ const SideIconButton = styled(IconButton)`
   float: right;
 `;
 
+const Loading = styled(CircularProgress)`
+  text-align: center;
+  width: 100%;
+`;
+
 const RoomComponent: React.FC = () => {
   const roomState = useSelector<State, RoomState>((state) => state.roomState);
+  const teamState = useSelector<State, TeamState>((state) => state.teamState);
   const dispatch = useDispatch();
 
   useEffect(() => {
     getRooms(dispatch);
+  }, [dispatch]);
+
+  useEffect(() => {
+    getTeams(dispatch);
   }, [dispatch]);
 
   const [filter, setFilter] = useState<string>('');
@@ -47,8 +59,8 @@ const RoomComponent: React.FC = () => {
   }, []);
 
   const roomsComponent = useMemo(() => {
-    if (roomState.isLoading) {
-      return <p>Loading ...</p>;
+    if (roomState.isLoading || teamState.isLoading) {
+      return <Loading />;
     }
     return roomState.rooms
       .filter((x) => x.title.toLowerCase().includes(filter.toLowerCase()))
@@ -57,17 +69,17 @@ const RoomComponent: React.FC = () => {
           <SideIconButton size="small">
             <MoreHoriz />
           </SideIconButton>
+          <Title>Team Name</Title>
+          <div>{teamState.teams.find((t) => t.id === room.teamId)?.name ?? '-'}</div>
           <Title>Room Name</Title>
           <div>{room.title}</div>
           <Title>Type</Title>
           <div>{room.type}</div>
           <Title>Room ID</Title>
           <Mono>{room.id}</Mono>
-          <Title>Team ID</Title>
-          <Mono>{room.teamId}</Mono>
         </ListItem>
       ));
-  }, [roomState, filter]);
+  }, [roomState.isLoading, teamState.isLoading, filter]);
 
   return (
     <div>
